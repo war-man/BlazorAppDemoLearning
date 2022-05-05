1f37b9e3eb5f862375913f56898ec16603505d06
war-man <Trumgaywar99@gmail.com>
2022-05-05 23:14:44 +0700

    Cty

--- BlazorAppDemo/Models/Contact.cs
+++ BlazorAppDemo/Models/Contact.cs
@@ -3,8 +3,10 @@
     [Serializable]
     public class Contact
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string GetFullName() => this.FirstName + " " + this.LastName;
+        public override string ToString() => $"Id: {FirstName}\r\nName: {LastName}\r\nAge: {Email}";
     }
 }
--- BlazorAppDemo/Pages/Index.razor.cs
+++ BlazorAppDemo/Pages/Index.razor.cs
@@ -1,6 +1,46 @@
-﻿namespace BlazorAppDemo.Pages
+﻿using BlazorAppDemo.Models;
+
+namespace BlazorAppDemo.Pages
 {
     public partial class Index
     {
+        private List<string>? name = null;
+        private List<Contact>? Contacts = null;
+        private Dictionary<string, object> MyTextBoxAttributes = new()
+        {
+            { "disabled", "disabled" },
+            { "placeholder", "New Contact 02" },
+            { "type", "text" }
+        };
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.Delay(5000);
+            Contacts = new()
+            {
+                new() { FirstName = "John", LastName = "Kevin", Email = "K@gmail.com" },
+                new() { FirstName = "John1", LastName = "Kevin1", Email = "K1@gmail.com" },
+                new() { FirstName = "John2", LastName = "Kevin2", Email = "K2@gmail.com" }
+
+            };
+            await base.OnInitializedAsync();
+        }
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            name = new()
+            {
+                "Base1",
+                "Base1",
+                "Base1",
+                "Base1",
+                "Base1",
+                "Base1",
+            };
+        }
+        private string username = "john";
+        private int TestNum { get; set; } = 1;
+        private string TernaryOperator = "Raw";
+        private string Getusername() => $"{username} Thomas";
+        private void ChangeName() => username = "Peter";
     }
 }
